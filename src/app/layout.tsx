c08cd677d13d43c303626cae70f09f0bfa82856d
Ben Ng <mr.ng@mystudies.net>
2026-04-27 21:11:47 +0800

    v1.96

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,11 +39,12 @@ export const metadata: Metadata = {
   description: APP_DESCRIPTION,
   icons: {
     icon: [
+      { url: "/favicon.ico", sizes: "48x48" },
       { url: "/icon-192x192.png", sizes: "192x192", type: "image/png" },
       { url: "/icon-512x512.png", sizes: "512x512", type: "image/png" },
     ],
     apple: [
-      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+      { url: "/apple-touch-icon.png", sizes: "180x180" },
     ],
   },
   appleWebApp: {
