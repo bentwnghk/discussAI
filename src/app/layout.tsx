b5d5f261cb79a79cd767b6e4136abffadb92bb4b
Ben Ng <mr.ng@mystudies.net>
2026-05-13 10:55:25 +0800

    v1.127

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,11 +13,13 @@ const poppins = Poppins({
   variable: "--font-poppins",
   subsets: ["latin"],
   weight: ["400", "500", "600", "700", "800", "900"],
+  preload: false,
 });
 
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
+  preload: false,
 });
 
 export const viewport: Viewport = {
